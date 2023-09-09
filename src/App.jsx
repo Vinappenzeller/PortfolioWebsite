5d285174daf91f88f974b4c1c6e193e04bdca110
Deezer <vin_appenzeller@outlook.de>
2023-09-10 01:03:18 +0200

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const notify = () => {
-    toast.info('To switch the theme click the moon 🌙', {
+    toast.info('To switch the theme click on the moon 🌙', {
       position: "top-center",
       autoClose: 6000,
       hideProgressBar: false,
